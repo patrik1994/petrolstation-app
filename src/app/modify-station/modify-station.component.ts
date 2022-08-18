2ac36aa570fc5b6860ab90ca13f019bbbfb08117
Patrik Nagyfeju <npatrik1994@gmail.com>
2022-08-18 16:11:52 +0200

    buttons from modify-station call a function

--- src/app/modify-station/modify-station.component.ts
+++ src/app/modify-station/modify-station.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { STATIONS } from '../mock-stations';
+import { Station } from '../station';
 
 @Component({
   selector: 'app-modify-station',
@@ -7,15 +9,30 @@ import { STATIONS } from '../mock-stations';
   styleUrls: ['./modify-station.component.css']
 })
 export class ModifyStationComponent implements OnInit {
-
+  station!: Station | undefined;
   fuelTypes = ["95-ös benzin", "Diesel", "98-as benzin", "100-as benzin", "95-ös prémium benzin", "Prémium diesel"];
 
   stations = STATIONS;
-  currentStation = this.stations[12];
-
-  constructor() { }
+  
+  constructor( private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const stationIdFromRoute = Number(routeParams.get('stationId'));
+
+    this.station = this.stations.find(
+      (station: { id: number; }) => station.id === stationIdFromRoute
+    );
+
   }
+
+  outOfFuel(_input: any) {
+    console.log("outOfFuel:" + _input);
+  }
+
+  thereIsFuel(_input: any) {
+    console.log("thereIsFuel:" + _input);
+  }
+
 
 }
