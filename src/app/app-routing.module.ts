6a2856c6742cb4301f30b8a9b526bea6320960dc
Csaba Somogyi <csaba.somogyi10@gmail.com>
2022-08-18 09:14:49 +0200

    fix route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ModifyStationComponent } from './modify-station/modify-station.component';
+import { StationsComponent } from './stations/stations.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: StationsComponent },
+    { path: 'stations/:stationId', component: ModifyStationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
