9bdaf6d6acfa95605cc8e03e3ef5bb0c5586d60d
Csaba Somogyi <csaba.somogyi10@gmail.com>
2022-09-03 02:15:59 +0200

    filter

--- src/app/stations/stations.component.ts
+++ src/app/stations/stations.component.ts
@@ -1,13 +1,13 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { PetrolStationService } from 'src/OpenApi/services';
-import { FormControl } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { DecimalPipe } from '@angular/common';
 import { MatTableDataSource } from '@angular/material/table';
 
 import { Observable, of } from 'rxjs';
-import { count, map, startWith } from 'rxjs/operators';
+import { count, debounceTime, distinctUntilChanged, map, startWith } from 'rxjs/operators';
 import { PetrolStationWithStatusesViewDto } from 'src/OpenApi/models/petrol-station-with-statuses-view-dto';
 import { StationContainerService } from '../station-container.service';
 import { MatSort } from '@angular/material/sort';
@@ -28,18 +28,32 @@ export class StationsComponent implements OnInit, AfterViewInit {
   dataSource = new MatTableDataSource<PetrolStationWithStatusesViewDto>([]);
   displayedColums = ['city',"street"];
 
+  filterForm?: FormGroup;
+
   length = 100;
   pageIndex = 0;
   pageSize = 10;
   pageSizeOptions: number[] = [5, 10, 25, 100];
 
   // MatPaginator Output
-  pageEvent: PageEvent;
+  pageEvent?: PageEvent;
+
+  public doFilter = (event: Event ) => {
+
+    this.dataSource.filter = (event.target as HTMLInputElement).value.trim().toLocaleLowerCase();
+  }
 
   constructor(
+    private fb: FormBuilder,
     private petrolStationService: PetrolStationService,
     private stationContainerService: StationContainerService
-  ) {}
+  ) {
+
+    this.filterForm = this.fb.group({
+    city: [],
+    street: []
+  });
+}
 
   ngOnInit(): void {
     this.refreshPetrolStations();
@@ -49,6 +63,30 @@ export class StationsComponent implements OnInit, AfterViewInit {
     this.sort.disableClear = true;
     this.dataSource.sort = this.sort;
 
+    this.dataSource.filterPredicate = this.filterPredicate;
+    this.filterForm.valueChanges.pipe(
+      startWith(''),
+      debounceTime(200),
+      distinctUntilChanged()).subscribe(() => this.dataSource.filter = '#');
+  }
+
+  filterPredicate = (data: PetrolStationWithStatusesViewDto, _: string): boolean => {
+
+    const { city, street } = this.filterForm.getRawValue();
+
+    if (city) {
+      const value = city.toLowerCase();
+      if (data.city.toLowerCase().includes(value) == false)
+        return false;
+    }
+
+    if (street) {
+      const value = street.toLowerCase();
+      if (data.street.toLowerCase().includes(value) == false)
+        return false;
+    }
+
+    return true;
   }
 
   refreshPetrolStations() {
@@ -61,6 +99,10 @@ export class StationsComponent implements OnInit, AfterViewInit {
         (this.dataSource.data = petrolStations.petrolStations!),
           (this.dataSource.data.length = petrolStations.totalCount!);
       });
+  }
+
+  resetFilters() {
+    this.filterForm?.reset();
   }
 
   public getServerData(event?: PageEvent) {
